8ad6aec77323d9c8de946bae3ec9905f2a214cd3
ci001216 <ci001216@gmail.com>
2017-09-13 13:30:25 +0200

    formularios

--- hola-mundo2/src/app/app.module.ts
+++ hola-mundo2/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CommonsModule } from './commons/commons.module';
+import { FormularioComponent } from './formulario/formulario.component';
+import { IdeasComponent } from './ideas/ideas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    IdeasComponent
   ],
   imports: [
     BrowserModule,
-    CommonsModule
+    CommonsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
